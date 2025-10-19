d40e340d94a8dbb3835d71771310c6982b9f31b2
jamiebyer <byerjamie@gmail.com>
2025-10-19 10:13:00 -0600

    edit main

--- src/main.py
+++ src/main.py
@@ -16,27 +16,26 @@ from inversion.inversion import Inversion
 
 if __name__ == "__main__":
 
-    # max_path = "./data/WH01/max_files/WH01_fine.max"
+    max_path = "./data/WH01/max_files/WH01_fine.max"
     # txt_path = "./data/WH01/txt_files/WH01_curve_fine.txt"
 
-    max_path = "./data/WH02/max_files/WH02_fine.max"
+    # max_path = "./data/WH02/max_files/WH02_fine.max"
     # txt_path = "./data/WH02/txt_files/WH02_curve_fine.txt"
 
     # max_path = "./capon-importedsignals.max"
 
-    # plot_geopsy_dispersion_curve(max_path, txt_path)
-    # plot_computed_dispersion_curve(max_path, f_min=-1, f_max=100000)
-    # plot_computed_dispersion_curve(max_path, f_range=[[2.2, 6]])  # WH01
+    # plot full curve
+    plot_computed_dispersion_curve(max_path, f_range=[[2.2, 7]], err_thresh=6)  # WH01
     # plot_computed_dispersion_curve(max_path, f_range=[[2, 3.7], [9.8, 10000]])  # WH02
 
+    # plot individual frequencies
     # plot_dispersion_curve_frequency(
-    #     max_path, f_range=[[2.2, 6]], freq=6
+    #     max_path, f_range=[[2.2, 7]], freq=2.2
     # )
-
-    plot_dispersion_curve_frequency(
-        max_path, f_range=[[2, 3.7], [9.8, 10000]], freq=10.5
-    )
+    # plot_dispersion_curve_frequency(
+    #     max_path, f_range=[[2, 3.7], [9.8, 10000]], freq=10.5
+    # )
 
     # compare_dispersion_curves(max_path, txt_path, f_min=2.2, f_max=7.5)
 
-    # run_geopsy()
+    # plot_site_maps()
